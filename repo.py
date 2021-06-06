e4c80885025803a793a8fb12347916abf13df33f
Tony <1462668901@qq.com>
2021-06-06 16:15:48 +0800

    Update repo.py

--- repo.py
+++ repo.py
@@ -1,1 +1,16 @@
-
+#!/usr/bin/env python3
+print('1.Africa')
+print('2.Asia')
+option = input('\nChoose your continent:')
+if int(option) == 1:
+    print('\n1.Kenya')
+    option = input('\nChoose your contry:')
+    try int(option) == 1:
+        print('\n1.Liquid Telecom')
+        break
+    except:
+        print('Invaid input!')
+        
+elif int (option) == 2:
+    print('\n1.China')
+    print('2.Japan')
